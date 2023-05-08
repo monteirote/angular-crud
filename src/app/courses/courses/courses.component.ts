8e29d3293d66fe43bbeb3d76b37260e6d29dcc29
Vinícius Monteiro <vinicius@iconax.com.br>
2023-05-08 12:08:41 -0300

    aulas 5 - 7

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'TypeScript',
+      category: 'FrontEnd',
+    }
+  ];
+  displayedColumns = ['name', 'category']
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
