64604dcef828d277c9b3a10a87f7d7edab2a483d
Vinícius Monteiro <vinicius@iconax.com.br>
2023-05-09 12:24:03 -0300

    aulas 10-12

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -15,12 +17,26 @@ export class CoursesComponent implements OnInit {
   displayedColumns = ['name', 'category']
 
 
-  constructor(private coursesService: CoursesService) {
-    // this.courses = this.coursesService.list();
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+  ) {
+    // this.courses = this. coursesService.list();
 
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.')
+          return of([])
+        })
+      );
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 
   ngOnInit(): void {
   }
