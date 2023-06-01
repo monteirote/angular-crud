becfc2638c8680db5f73fb357be4b1c70fe5b0c4
Vinícius Monteiro <vinicius@iconax.com.br>
2023-06-01 17:17:25 -0300

    aula 34

--- src/app/courses/containers/courses/course-form/course-form.component.ts
+++ src/app/courses/containers/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { CoursesService } from '../../../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -12,11 +12,13 @@ import { Course } from 'src/app/courses/model/course';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
-
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: [''],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+    ],
+    category: ['', [Validators.required]],
   });
 
   ngOnInit(): void {
@@ -25,7 +27,7 @@ export class CourseFormComponent implements OnInit {
       name: course.name,
       category: course.category,
       _id: course._id,
-    })
+    });
   }
 
   constructor(
@@ -33,15 +35,13 @@ export class CourseFormComponent implements OnInit {
     private location: Location,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private route: ActivatedRoute,
-  ) {
-
-  }
+    private route: ActivatedRoute
+  ) {}
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
-      result => this.onSucess(),
-      error => this.onError(),
+      (result) => this.onSucess(),
+      (error) => this.onError()
     );
   }
 
@@ -50,11 +50,34 @@ export class CourseFormComponent implements OnInit {
   }
 
   private onError() {
-    this.snackBar.open('Erro ao salvar curso.','', {duration: 3000});
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
 
   private onSucess() {
-    this.snackBar.open('Curso salvo com sucesso!','', {duration: 3000});
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
     this.location.back();
+  }
+
+  getErrorMessage(fieldName: string) {
+
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+      const requiredLength = field.errors ?
+                                field.errors['minlength']['requiredLength'] : 3;
+      return `Nome curto demais (Min. de ${requiredLength} caracteres)`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredLength = field.errors ?
+                                field.errors['maxlength']['requiredLength'] : 30;
+      return `Nome longo ${requiredLength} demais (Max. de ${requiredLength} caracteres).`;
+    }
+
+    return 'Campo inválido';
   }
 }
